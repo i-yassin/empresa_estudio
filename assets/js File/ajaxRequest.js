c0fd978cbae791ef1fb0d894029c5e5b5f3a8c7f
Mohammad Abu Nabaa <m.nabaaa@gmail.com>
2018-10-16 03:19:14 +0300

    redo task will enhanced css style

--- assets/js File/ajaxRequest.js
+++ assets/js File/ajaxRequest.js
@@ -1,22 +1,35 @@
 const requestTimeOut = 1000*5;
-
-function getData(url){
-  $.ajax({
-      url:url,
+function getData() {
+  $.ajax ({
+      url: "https://api.myjson.com/bins/izt2s",
       dataType: 'json',
       timeout: requestTimeOut,
-      success: function(resp) { 
-        if(resp.status == true) {
-            alert("its loaded fine");
-          }
+      success: function(response) { 
+        if(response.status == true) {
+                var pageURL= $(location).attr("href").toString();
+             $.each(response.roomsData, function(index, room) {
+                $('#where-image-appear').append(
+                '<div class="col-sm-6 col-md-4 col-lg-3 image-container " >'+
+                    '<div class="imagestyle" >'+
+                        '<img src="./assets/images/'+room.image+'"  class="img-fluid image-filteration image-zoom" alt="'+room.name+'" onclick="appearingModal(this)"/>'+  
+                        '<div class="overlay">'+
+                            '<div class="text">'+
+                            room.name+
+                            '</div>'+
+                        '</div>'+
+                    '</div>'+
+                '</div>');
+             });
+        }
         },
+
         error: function(req, status,error) {
             if(status==="timeout") {
                 alert("got timeout");
-                
             }
-            else
+            else{
                 console.log(status);
+            }
          }
   });
 }
