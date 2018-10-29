a4042d70ccd27dbb4753096918a228f68706ce13
Mohammad Abu Nabaa <m.nabaaa@gmail.com>
2018-10-29 12:14:42 +0200

    adding some style and design

--- assets/js/ajaxRequest.js
+++ assets/js/ajaxRequest.js
@@ -84,7 +84,7 @@ function FilterData(wantedItems,filterdBy){
                         }                    
                 }
                 else if(filterdBy === 'style') {
-                    if(wantedItems == room.style){
+                    if(wantedItems.toLowerCase() == room.style.toLowerCase()){
                         $('#where-image-appear').append(
                         '<div class= "col-sm-6 col-md-4 col-lg-3 image-container" id="id'+ room.id +'">'+
                             '<div class= "imagestyle">'+
